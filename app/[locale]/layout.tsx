8c91891460ba795ff7d2bbf3d0b5f92b8a4d075f
Igor Gusakov <igorgusakov199918@gmail.com>
2023-10-10 18:27:48 +0600

    end fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,31 @@
+// @ts-ignore
 import "@/public/scss/styles.scss";
 import { notFound } from "next/navigation";
-import { NextIntlClientProvider } from "next-intl";
+import { NextIntlClientProvider, useTranslations } from "next-intl";
 import Head from "next/head";
+import { Metadata } from "next";
+
+export const generateMetadata = ({ params }: { params: { locale: 'ru' | 'kk' | 'en' } }): Metadata => {
+   const titleRu = 'Пробное онлайн-тестирование для поступления в НИШ и РФМШ';
+   const titleEn = 'NIS and RFMS online trial admission tests';
+   const titleKk = 'НЗМ және РФММ-ге түсу үшін сынама онлайн-тестілеу';
+ 
+   let title: string;
+ 
+   if (params.locale === 'ru') {
+     title = titleRu;
+   } else if (params.locale === 'en') {
+     title = titleEn;
+   } else if (params.locale === 'kk') {
+     title = titleKk;
+   } else {
+     title = 'Synaq';
+   }
+ 
+   return {
+     title,
+   };
+ };
 
 export default async function LocaleLayout({
    children,
@@ -14,11 +38,13 @@ export default async function LocaleLayout({
       notFound();
    }
 
+   
+
    return (
 
       <html lang={locale}>
-         <Head>
-            <title></title>
+         <Head>            
+            {/* <title>{locale === 'ru' ? titleRu : locale === 'en' ? titleEn : titleKk}</title> */}
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link
                rel="preconnect"
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,12 +2,23 @@ import { useTranslations } from "next-intl";
 import LandingHeader from "@/components/Header/LandingHeader";
 import BodyMiddle from "@/components/Body/BodyMiddle";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head";
 
 export default function Home() {
    const t = useTranslations("Index");
 
    return (
       <>
+       <Head>
+        <title>
+          iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple
+        </title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
+        />
+      </Head>
          <LandingHeader />
          <BodyMiddle />
          <Footer />
